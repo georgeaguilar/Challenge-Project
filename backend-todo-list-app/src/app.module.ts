9157cb5a14672223abeb129b69c6e0528e91f7e1
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-14 14:33:23 -0600

    create tasks module, controller and service

--- backend-todo-list-app/src/app.module.ts
+++ backend-todo-list-app/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { TaksService } from './taks/taks.service';
 
 @Module({
-  imports: [],
+  imports: [TasksModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, TaksService],
 })
 export class AppModule {}
