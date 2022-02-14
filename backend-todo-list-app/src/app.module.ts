d39ae7e7da498a8e873319c23339aecbb3427d2d
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-14 15:09:54 -0600

    making tasks controllers

--- backend-todo-list-app/src/app.module.ts
+++ backend-todo-list-app/src/app.module.ts
@@ -2,11 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
-import { TaksService } from './taks/taks.service';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [TasksModule],
   controllers: [AppController],
-  providers: [AppService, TaksService],
+  providers: [AppService, TasksService],
 })
 export class AppModule {}
--- backend-todo-list-app/src/tasks/tasks.controller.ts
+++ backend-todo-list-app/src/tasks/tasks.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+
+    @Get()
+    async getTasks(){
+        return 'all the tasks';
+    }
+
+    @Get(':id')
+    async getTask(@Param('id') id: string){
+        return `task ${id}`;
+    }
+
+    @Post()
+    async createTask(taskDto: any){
+        return "create a task";
+    }
+
+    @Put(':id')
+    async updateTask(@Param('id') id: string){
+        return 'edit task';
+    }
+
+    async deleteTask(@Param('id') id: string){
+        return id;
+    }
+}
