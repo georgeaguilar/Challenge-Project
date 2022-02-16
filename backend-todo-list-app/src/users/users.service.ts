67539a9923ebe8d3b027c99be9b4d0d6c3c84627
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-16 13:58:54 -0600

    making auth and users

--- backend-todo-list-app/src/users/users.service.ts
+++ backend-todo-list-app/src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
 
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      email: 'georgeaguilar11@hotmail.com',
+      password: '123',
+    },
+    {
+      userId: 2,
+      email: 'carlosaguilar11@hotmail.com',
+      password: '123',
+    },
+  ];
+
+  async findOne(email: string): Promise<User> {
+    return this.users.find((user) => user.email === email);
+  }
+}
