bcec9b253491d012068b5cc7385c65a0d7094b5b
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-16 00:28:46 -0600

    customizing tasks controller

--- backend-todo-list-app/src/tasks/tasks.controller.ts
+++ backend-todo-list-app/src/tasks/tasks.controller.ts
@@ -3,10 +3,13 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Post,
   Put,
+  Res,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { UpdateTaskDto } from './dtos/update-task.dto';
 import { TasksService } from './tasks.service';
@@ -16,27 +19,51 @@ export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Get()
-  async getTasks() {
-    return await this.tasksService.getTasks();
+  async getTasks(@Res() res: Response) {
+    const tasks = await this.tasksService.getTasks();
+    return res.status(HttpStatus.OK).json({
+      message: 'stored tasks',
+      tasks,
+    });
   }
 
   @Get(':id')
-  async getTask(@Param('id') id: string) {
-    return await this.tasksService.getTask(id);
+  async getTask(@Param('id') id: string, @Res() res: Response) {
+    const task = await this.tasksService.getTask(id);
+    return res.status(HttpStatus.OK).json({
+      message: 'stored task',
+      task,
+    });
   }
 
   @Post()
-  async createTask(@Body() taskDto: CreateTaskDto) {
-    return await this.tasksService.createTask(taskDto);
+  async createTask(@Body() taskDto: CreateTaskDto, @Res() res: Response) {
+    const task = await this.tasksService.createTask(taskDto);
+    return res.status(HttpStatus.OK).json({
+      message: 'created task',
+      task,
+    });
   }
 
   @Put(':id')
-  async updateTask(@Param('id') id: string, @Body() taskDto: UpdateTaskDto) {
-    return await this.tasksService.updateTask(id, taskDto);
+  async updateTask(
+    @Param('id') id: string,
+    @Body() taskDto: UpdateTaskDto,
+    @Res() res: Response,
+  ) {
+    const task = await this.tasksService.updateTask(id, taskDto);
+    return res.status(HttpStatus.OK).json({
+      message: 'updated task',
+      task,
+    });
   }
 
   @Delete(':id')
-  async deleteTask(@Param('id') id: string) {
-    return await this.tasksService.deleteTask(id);
+  async deleteTask(@Param('id') id: string, @Res() res: Response) {
+    const task = await this.tasksService.deleteTask(id);
+    return res.status(HttpStatus.OK).json({
+      message: 'deleted task',
+      task,
+    });
   }
 }
