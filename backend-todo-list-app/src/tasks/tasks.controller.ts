0c9e61f498f7a8c9d9e1c71e3058c8668749bdbb
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-15 19:21:12 -0600

    making tasks services

--- backend-todo-list-app/src/tasks/tasks.controller.ts
+++ backend-todo-list-app/src/tasks/tasks.controller.ts
@@ -1,31 +1,42 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateTaskDto } from './dtos/create-task.dto';
 import { UpdateTaskDto } from './dtos/update-task.dto';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
   @Get()
   async getTasks() {
-    return 'all the tasks';
+    return await this.tasksService.getTasks();
   }
 
   @Get(':id')
   async getTask(@Param('id') id: string) {
-    return `task ${id}`;
+    return await this.tasksService.getTask(id);
   }
 
   @Post()
-  async createTask(taskDto: CreateTaskDto) {
-    return taskDto;
+  async createTask(@Body() taskDto: CreateTaskDto) {
+    return await this.tasksService.createTask(taskDto);
   }
 
   @Put(':id')
-  async updateTask(@Param('id') id: string, taskDto: UpdateTaskDto) {
-    return taskDto;
+  async updateTask(@Param('id') id: string, @Body() taskDto: UpdateTaskDto) {
+    return await this.tasksService.updateTask(id, taskDto);
   }
 
   @Delete(':id')
   async deleteTask(@Param('id') id: string) {
-    return id;
+    return await this.tasksService.deleteTask(id);
   }
 }
--- backend-todo-list-app/src/tasks/tasks.service.ts
+++ backend-todo-list-app/src/tasks/tasks.service.ts
@@ -1,4 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateTaskDto } from './dtos/create-task.dto';
+import { UpdateTaskDto } from './dtos/update-task.dto';
+import { ITask } from './interfaces/task.interface';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(@InjectModel('tasks') private readonly taskModel: Model<ITask>) {}
+
+  async getTasks(): Promise<ITask[]> {
+    return await this.taskModel.find();
+  }
+
+  async getTask(id: string): Promise<ITask> {
+    const task = await this.taskModel.findById(id);
+
+    if (!task) throw new NotFoundException(`id ${id} not found`);
+
+    return task;
+  }
+
+  async createTask(taskDto: CreateTaskDto): Promise<ITask> {
+    const task = new this.taskModel(taskDto);
+    return await task.save();
+  }
+
+  async updateTask(id: string, taskDto: UpdateTaskDto): Promise<ITask> {
+    const task = await this.taskModel.findByIdAndUpdate(id, taskDto, {
+      new: true,
+    });
+
+    if (!task) throw new NotFoundException(`id ${id} not found`);
+
+    return task;
+  }
+
+  async deleteTask(id: string): Promise<ITask> {
+    const task = await this.taskModel.findByIdAndDelete(id);
+
+    if (!task) throw new NotFoundException(`id ${id} not found`);
+
+    return task;
+  }
+}
