be5534d369bd440875243fe2ce9caa9737c4b65c
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-16 11:55:27 -0600

    created auth and users

--- backend-todo-list-app/src/app.module.ts
+++ backend-todo-list-app/src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 import { MongoModule } from './config/mongo.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TasksModule, MongoModule],
+  imports: [TasksModule, MongoModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
