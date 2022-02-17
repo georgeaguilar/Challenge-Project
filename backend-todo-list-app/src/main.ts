9f3100c223817536b90a89322407b79bbc2ba742
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-17 14:18:27 -0600

    the server running now localhost:5000

--- backend-todo-list-app/src/main.ts
+++ backend-todo-list-app/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const logger = new Logger();
 
-  await app.listen(3000);
+  await app.listen(5000);
 
   logger.log(`Server running in: ${await app.getUrl()}`);
 }
