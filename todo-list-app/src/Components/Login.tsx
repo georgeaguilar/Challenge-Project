e9e350e7c92e53b0f4231c70731ace02cc4fc7c7
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-18 18:40:43 -0600

    allow users to login

--- todo-list-app/src/Components/Login.tsx
+++ todo-list-app/src/Components/Login.tsx
@@ -1,6 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  function login(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    setError("");
+
+    axios
+      .post("http://localhost:5000/auth/login", {
+        email,
+        password,
+      })
+      .then(
+        (response) => {
+          localStorage.setItem("accessToken", response.data.access_token);
+          navigate("/");
+        },
+        (error) => {
+          setError(error.response.data.message);
+        }
+      );
+  }
+
   return (
     <div
       className="card 
@@ -8,17 +35,32 @@ const Login: React.FC = () => {
     >
       <div className="card-body">
         <h5 className="card-title text-center">Login</h5>
-        <form>
+        {error && (
+          <div className="alert alert-danger" role="alert">
+            {error}
+          </div>
+        )}
+        <form onSubmit={login}>
           <div className="mb-3">
             <label className="form-label">Email address</label>
-            <input type="text" className="form-control" />
+            <input
+              type="email"
+              placeholder="Email address"
+              className="form-control"
+              onChange={(e) => setEmail(e.target.value)}
+            />
             <div className="form-text">
               We'll never share your email with anyone else.
             </div>
           </div>
           <div className="mb-3">
             <label className="form-label">Password</label>
-            <input className="form-control" />
+            <input
+              type="password"
+              placeholder="Password"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
           <button type="submit" className="btn btn-primary">
             Login
