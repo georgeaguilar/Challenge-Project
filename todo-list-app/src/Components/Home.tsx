64c9ddd5ba156f03f801ce044e5279998458af6c
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-19 00:39:34 -0600

    allow user to logout

--- todo-list-app/src/Components/Home.tsx
+++ todo-list-app/src/Components/Home.tsx
@@ -1,9 +1,31 @@
+import { Navigate, useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
+import { useEffect } from "react";
+import Tasks from "./Tasks";
 
 const Home: React.FC = () => {
+  const hasAccessToken = Boolean(localStorage.getItem("accessToken"));
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!hasAccessToken) {
+      return;
+    }
+  }, [hasAccessToken]);
+
+  function logout() {
+    localStorage.removeItem("accessToken");
+    navigate("/login");
+  }
+
+  if (!hasAccessToken) {
+    return <Navigate to="/login" />;
+  }
+
   return (
     <div>
-      <Navbar />
+      <Navbar logout={logout} />
+      <Tasks />
     </div>
   );
 };
