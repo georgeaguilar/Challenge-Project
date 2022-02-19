8dd9b1e72a7795276f8091bab68b44f40b80d0c8
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-19 00:52:15 -0600

    create API

---
+++ todo-list-app/src/API/api.tsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const api = axios.create({ baseURL: "http://localhost:5000" });
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  config.headers = { Authorization: `Bearer ${token}` };
+  return config;
+});
+
+api.interceptors.response.use(
+  (response) => {
+    console.log("success:", response.status);
+    return response;
+  },
+  (error) => {
+    console.log("error:", error.response.status);
+    if (error.response.status === 401) {
+      localStorage.removeItem("accessToken");
+      window.location.href = "/login";
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
