8c234aa00a3d09c57fed39d396a144bf59405c2c
georgeaguilar <georgeaguilar@htoamil.com>
2022-02-20 01:36:45 -0600

    set enviroment variable for mongo

--- backend-todo-list-app/src/config/mongo.module.ts
+++ backend-todo-list-app/src/config/mongo.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+const mongoDB = process.env.MONGODB_URI || 'mongodb://localhost/api';
+
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/api')],
+  imports: [MongooseModule.forRoot(mongoDB)],
 })
 export class MongoModule {}
